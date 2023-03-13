17cfb336a88b5f87c5b434bc56011e2502c21214
Joseph Baek <josephbaek97@gmail.com>
2023-03-13 16:01:43 -0700

    Added 4 arrays of characters for the password

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,20 @@
 // Assignment code here
+// Arrays
+// Lowercase
+letters = "abcdefghijklmnopqrstuvwxyz"
+lowercaseArray = letters.split("");
+
+
+// Uppercase
+capitalLetters = letters.toUpperCase();
+uppercaseArray = capitalLetters.split("");
+
+// Numeric
+numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Special Characters. This list of characters was found on "https://owasp.org/www-community/password-special-characters". I included all the characters in the list except for the "space" character since most generated passwords don't include the "space" character.
+specialCharactersArray = ["!", `"`, "#", "$", "%", "&", `'`, "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^" , "_", "`", "{", "|", "}", "~" ];
+
 
 
 // Get references to the #generate element
