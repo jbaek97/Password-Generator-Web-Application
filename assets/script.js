47f40dd415e335befcc40ecc46ac06fb9f12da2e
Joseph Baek <josephbaek97@gmail.com>
2023-03-13 17:34:11 -0700

    Corrected the syntax of the arrays and added the character length part of the generatePassword function

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,28 @@
 // Assignment code here
 // Arrays
 // Lowercase
-letters = "abcdefghijklmnopqrstuvwxyz"
-lowercaseArray = letters.split("");
+var letters = "abcdefghijklmnopqrstuvwxyz"
+var lowercaseArray = letters.split("");
 
 
 // Uppercase
-capitalLetters = letters.toUpperCase();
-uppercaseArray = capitalLetters.split("");
+var capitalLetters = letters.toUpperCase();
+var uppercaseArray = capitalLetters.split("");
 
 // Numeric
-numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Special Characters. This list of characters was found on "https://owasp.org/www-community/password-special-characters". I included all the characters in the list except for the "space" character since most generated passwords don't include the "space" character.
-specialCharactersArray = ["!", `"`, "#", "$", "%", "&", `'`, "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^" , "_", "`", "{", "|", "}", "~" ];
+var specialCharactersArray = ["!", `"`, "#", "$", "%", "&", `'`, "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^" , "_", "`", "{", "|", "}", "~" ];
 
+function generatePassword() {
+  //Character Amount Prompt
+  var characters = prompt("Choose a length of the password. The password can be anywhere from at least 8 characters to no more than 128 characters:", "0");
+  // Character Amount 
+  if (JSON.parse(characters) < 8 || JSON.parse(characters) > 128) {
+    alert("You have selected an invalid amount of characters. Please try again");
+    return "Invalid input. Please try again."
+  } else if( 7 < JSON.parse(characters) < 129) {}
 
 
 // Get references to the #generate element
